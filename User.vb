e243998284ac9786a889c9edf36036e40fc2712d
Charles Robinson <ccr112393@gmail.com>
2020-01-20 12:43:41 -0500

    Add project files.

---
+++ User.vb
@@ -0,0 +1,74 @@
+﻿<Serializable()> Public Class User
+
+    Private Property Username As String
+    Private Property Password As String
+    Private Property Fullname As String
+    Private Property Rank As String
+
+    Public Sub New(username As String, password As String, fullname As String, rank As String)
+        If username Is Nothing Then
+            Throw New ArgumentNullException(NameOf(username))
+        End If
+
+        If password Is Nothing Then
+            Throw New ArgumentNullException(NameOf(password))
+        End If
+
+        If fullname Is Nothing Then
+            Throw New ArgumentNullException(NameOf(fullname))
+        End If
+
+        If rank Is Nothing Then
+            Throw New ArgumentNullException(NameOf(rank))
+        End If
+
+        Me.Username = username
+        Me.Password = Crypto.GenerateSHA512String(password)
+        Me.Fullname = fullname
+        Me.Rank = rank
+
+    End Sub
+
+    ' Get Properties/Fuctions
+    Public Function getUsername() As String
+        Return Username
+    End Function
+
+    Public Function getPassword() As String
+        Return Password
+    End Function
+
+    Public Function getFullName() As String
+        Return Fullname
+    End Function
+
+    Public Function getRank() As String
+        Return Rank
+    End Function
+
+    Public Function getString() As String
+        Return String.Format("{0}, {1}, {2}", getUsername, getFullName, getRank)
+    End Function
+
+    Public Function getStringPassword() As String
+        Return String.Format("{0}, {1}, {2}, {3}", getUsername, getFullName, getRank, getPassword)
+    End Function
+
+    'Set Properties/Functions
+    Public Sub setUsername(username As String)
+        Me.Username = username
+    End Sub
+
+    Public Sub setPassword(password As String)
+        Me.Password = Crypto.GenerateSHA512String(password)
+    End Sub
+
+    Public Sub setFullName(fullname As String)
+        Me.Fullname = fullname
+    End Sub
+
+    Public Sub setRank(rank As String)
+        Me.Rank = rank
+    End Sub
+
+End Class
---
+++ UserManager.vb
@@ -0,0 +1,90 @@
+﻿Imports System.IO
+Imports System.Runtime.Serialization.Formatters.Binary
+
+<Serializable()> Public Class UserManager
+
+    Private Property AllUsers As List(Of User)
+
+    Public Sub New()
+        Me.AllUsers = New List(Of User)
+    End Sub
+
+    Public Sub newUser(username As String, password As String, fullname As String, rank As String)
+        'User class Hashses the password
+        AllUsers.Add(New User(username, password, fullname, rank))
+    End Sub
+
+    ' Get Functions
+    Public Function getUser(username As String) As User
+        Return AllUsers.Find(Function(usr As User) usr.getUsername = username)
+    End Function
+
+    Public Function getListOfUsers()
+        Return AllUsers
+    End Function
+
+    ' Save & Load DB
+
+    Public Function Save(filename As String)
+        If AllUsers Is Nothing Then
+            Throw New ArgumentNullException(NameOf(AllUsers))
+        End If
+
+        If filename Is Nothing Then
+            Throw New ArgumentNullException(NameOf(filename))
+        End If
+
+        Dim fstream As Stream = New FileStream(filename, FileMode.Create)
+        Dim bformatter As BinaryFormatter = New BinaryFormatter()
+        bformatter.Serialize(fstream, AllUsers)
+        fstream.Close()
+        Return True
+    End Function
+
+    Public Function Load(filename As String)
+        If filename Is Nothing Then
+            If filename Is Nothing Then
+                Throw New ArgumentNullException(NameOf(filename))
+            End If
+        End If
+
+        Dim fstream As Stream = New FileStream(filename, FileMode.Open)
+        Dim bformatter As BinaryFormatter = New BinaryFormatter()
+        AllUsers = CType(bformatter.Deserialize(fstream), List(Of User))
+        Return True
+    End Function
+
+    ' External List Of Users
+
+    'Public Function saveUserDB(allusers As List(Of User), filename As String)
+    '    If allusers Is Nothing Then
+    '        Throw New ArgumentNullException(NameOf(allusers))
+    '    End If
+
+    '    If filename Is Nothing Then
+    '        Throw New ArgumentNullException(NameOf(filename))
+    '    End If
+
+    '    Dim fstream As Stream = New FileStream(filename, FileMode.Create)
+    '    Dim bformatter As BinaryFormatter = New BinaryFormatter()
+    '    bformatter.Serialize(fstream, allusers)
+    '    fstream.Close()
+    '    Return True
+
+    'End Function
+
+    'Public Function loadUserDB(filename As String) As List(Of User)
+    '    If filename Is Nothing Then
+    '        If filename Is Nothing Then
+    '            Throw New ArgumentNullException(NameOf(filename))
+    '        End If
+    '    End If
+
+    '    Dim fstream As Stream = New FileStream(filename, FileMode.Open)
+    '    Dim bformatter As BinaryFormatter = New BinaryFormatter()
+    '    Dim allusers As List(Of User)
+    '    allusers = CType(bformatter.Deserialize(fstream), List(Of User))
+    '    Return allusers
+    'End Function
+
+End Class
